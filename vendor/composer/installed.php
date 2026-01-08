c46229aa2d583f544aed5be64a0168ea9013ed72
GitHub Action CI <actions@github.com>
2026-01-08 15:15:49 +0000

    built from 7ff683e989a1d506a66bb4f4fed3e066daef1c25

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'spenserhale/open-transposh',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7ff683e989a1d506a66bb4f4fed3e066daef1c25',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'spenserhale/open-transposh' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7ff683e989a1d506a66bb4f4fed3e066daef1c25',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
